65625c1a73eae99c48ba750809a591cf08e4b01e
PABLO HENRIQUE DE ANDRADE DA SILVA <pablo23102007@gmail.com>
2025-11-26 18:33:58 -0300

    Nutricionista, dieta, treino, adiçao de treino professor

--- front/frontend/src/app/modules/treino/treino.component.ts
+++ front/frontend/src/app/modules/treino/treino.component.ts
@@ -18,4 +18,40 @@ export class TreinoComponent {
     'Sabado',
     'Domingo'
   ];
+   treinos: any = {
+    Segunda: [
+      {
+        nome: "Rotação lateral das pernas",
+        repeticoes: "6 rep.",
+        imagem: "assets/exercicios/rotacao.png"
+      },
+      {
+        nome: "Abdominal invertido",
+        repeticoes: "8 rep.",
+        imagem: "assets/exercicios/invertido.png"
+      },
+      {
+        nome: "Abdominal em V",
+        repeticoes: "10 rep.",
+        imagem: "assets/exercicios/v.png"
+      }
+    ],
+    Terça: [
+      { nome: "Flexão", repeticoes: "15 rep.", imagem: "assets/exercicios/flexao.png" },
+      { nome: "Prancha", repeticoes: "2 min", imagem: "assets/exercicios/prancha.png" }
+    ],
+    Quarta: [],
+    Quinta: [],
+    Sexta: [],
+    Sábado: [],
+    Domingo: []
+  };
+
+  
+  treinoSelecionado: any[] = [];
+
+  selecionarDia(dia: string) {
+    this.treinoSelecionado = this.treinos[dia] || [];
+  }
 }
+
